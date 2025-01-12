60b78369f09383fd3e9374084335770b12634d42
Artur Yurchenko <artur24814@gmail.com>
2025-01-12 13:49:04 +0100

    Swager UI has been added

--- src/main/java/pl/vistula/firstrestapispring/FirstRestApiSpringApplication.java
+++ src/main/java/pl/vistula/firstrestapispring/FirstRestApiSpringApplication.java
@@ -1,9 +1,13 @@
 package pl.vistula.firstrestapispring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title = "Products API for Vistula", version = "1.0", description = "Product Information"))
 public class FirstRestApiSpringApplication {
 
 	public static void main(String[] args) {
