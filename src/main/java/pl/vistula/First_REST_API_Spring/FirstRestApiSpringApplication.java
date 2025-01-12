9906d7f0e2ffa9987c2f6b3aae1f8e82305f0a1a
Artur Yurchenko <artur24814@gmail.com>
2025-01-12 11:34:31 +0100

    The Spring project has been created

---
+++ src/main/java/pl/vistula/First_REST_API_Spring/FirstRestApiSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.vistula.First_REST_API_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/vistula/First_REST_API_Spring/FirstRestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vistula.First_REST_API_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
